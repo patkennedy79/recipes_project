7350d38a94ecd5d920b4b44078b7636d029a3938
Patrick Kennedy <patkennedy79@gmail.com>
2015-09-18 23:21:46 -0700

    Fix bug in wsgi.py where settings were not properly pointed to

--- family_recipes_app/wsgi.py
+++ family_recipes_app/wsgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_heroku_test.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "family_recipes_app.settings")
 
 application = Cling(get_wsgi_application())
