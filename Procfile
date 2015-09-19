4eac9ecc67e836a2c322be89f2f4bd213216cec9
Patrick Kennedy <patkennedy79@gmail.com>
2015-09-18 23:11:08 -0700

    Add files for deploying to Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn recipes_project.wsgi --log-file -
--- family_recipes_app/wsgi.py
+++ family_recipes_app/wsgi.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "family_recipes_app.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_heroku_test.settings")
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
